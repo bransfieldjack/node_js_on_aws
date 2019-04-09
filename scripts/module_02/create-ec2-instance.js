95336e29097803f9a5781e095483ec609b3488ca
Jack Bransfield <33059854+bransfieldjack@users.noreply.github.com>
2019-04-09 13:28:19 +1000

    Adding const variables and working on security group function.

---
+++ scripts/module_02/create-ec2-instance.js
@@ -0,0 +1,45 @@
+// Imports
+const AWS = require('aws-sdk')
+const helpers = require('./helpers')
+
+AWS.config.update({ region: 'us-east-1' })
+
+// Declare local variables
+const ec2 = new AWS.EC2()
+const sgName = 'hamster_sg'
+const keyName = 'hamster_key'
+
+// Do all the things together
+createSecurityGroup(sgName)
+.then(() => {
+  return createKeyPair(keyName)
+})
+.then(helpers.persistKeyPair)
+.then(() => {
+  return createInstance(sgName, keyName)
+})
+.then((data) => {
+  console.log('Created instance with:', data)
+})
+.catch((err) => {
+  console.error('Failed to create instance with:', err)
+})
+
+// Create functions
+
+function createSecurityGroup (sgName) {
+  const params = {
+    Description: sgName,
+    GroupName: sgName
+  }
+
+  
+}
+
+function createKeyPair (keyName) {
+  // TODO: Create keypair
+}
+
+function createInstance (sgName, keyName) {
+  // TODO: create ec2 instance
+}
